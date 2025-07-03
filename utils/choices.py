cc2d848aa1e4fcee838986aa94433f52708af965
Hyerin Sung <hyerinx@ewha.ac.kr>
2025-07-04 03:34:18 +0900

    🔨 ExerciseGoalType 인티저초이스 추가 (#20)

---
+++ utils/choices.py
@@ -0,0 +1,8 @@
+from django.db.models import IntegerChoices
+
+class ExerciseGoalType(IntegerChoices):
+    FIGURE = 1, '체형 관리'
+    HEALTH = 2, '건강 관리'
+    STRESS = 3, '스트레스 해소'
+    MUSCLE = 4, '근력 강화'
+    FLEXIBILITY = 5, '유연성 향상'
