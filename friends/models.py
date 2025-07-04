c20e243249bae0c1c2f6dc49d0c6decf0f564cfd
Hyerin Sung <hyerinx@ewha.ac.kr>
2025-07-05 03:04:58 +0900

    🔨 Friend 모델 추가 (#20)

--- friends/models.py
+++ friends/models.py
@@ -1,3 +1,36 @@
 from django.db import models
+from utils.choices import StatusType
+from accounts.models import CustomUser
 
-# Create your models here.
+class Friend(models.Model):
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+    )
+    updated_at = models.DateTimeField(
+        auto_now=True,
+    )
+    sender = models.ForeignKey(
+        CustomUser,
+        related_name='friend_sendings',
+        on_delete=models.CASCADE,
+    )
+    receiver = models.ForeignKey(
+        CustomUser,
+        related_name='friend_receivings',
+        on_delete=models.CASCADE,
+    )
+    status = models.PositiveSmallIntegerField(
+        choices=StatusType.choices,
+        default=1,
+    )
+
+    def __str__(self):
+        return f'{self.sender.email} → {self.receiver.email}'
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=['sender','receiver'],
+                name='친구 요청은 한 번만 합니다.',
+            )
+        ]
