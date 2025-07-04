cc07617c22c7c22434cd8f859e0448d3996d2421
Hyerin Sung <hyerinx@ewha.ac.kr>
2025-07-05 02:45:11 +0900

    🔨 ConditionReview 모델 추가 (#20)

--- conditionreviews/admin.py
+++ conditionreviews/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
+from .models import ConditionReview
 
-# Register your models here.
+admin.site.register(ConditionReview)
--- conditionreviews/models.py
+++ conditionreviews/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from accounts.models import CustomUser
 
-# Create your models here.
+class ConditionReview(models.Model):
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+    )
+    updated_at = models.DateTimeField(
+        auto_now=True,
+    )
+    user = models.ForeignKey(
+        CustomUser,
+        related_name='condition_reviews',
+        on_delete=models.CASCADE,
+    )
+    date = models.DateField()
+    rating = models.PositiveSmallIntegerField()
+    comment = models.TextField(
+        null=True,
+        blank=True,
+    )
+
+    def __str__(self):
+        return f'{self.user.email}: {self.date}'
+    
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=['user','date'],
+                name='사용자는 컨디션 리뷰를 하루에 한 번만 작성합니다.',
+            )
+        ]
