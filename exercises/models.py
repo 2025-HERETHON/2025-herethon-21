1b304838ae59d12d126a9852bd9aa270ba2885df
Hyerin Sung <hyerinx@ewha.ac.kr>
2025-07-05 02:58:29 +0900

    🔨 Exercise 모델 추가 (#20)

--- exercises/models.py
+++ exercises/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from utils.choices import ExerciseCategoryType
 
-# Create your models here.
+class Exercise(models.Model):
+    name = models.CharField(
+        max_length=20,
+        unique=True,
+    )
+    duration = models.DurationField()
+    category = models.PositiveSmallIntegerField(
+        choices=ExerciseCategoryType.choices
+    )
+    image = models.ImageField(
+        upload_to='exercise/image',
+    )
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
