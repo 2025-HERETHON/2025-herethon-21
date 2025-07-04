d4132ddfc755924b9ef6b5ef4f6c1101a70ccdb0
Hyerin Sung <hyerinx@ewha.ac.kr>
2025-07-05 02:59:14 +0900

    🔨 ExerciseHistory 모델 추가 (#20)

--- exercises/models.py
+++ exercises/models.py
@@ -1,5 +1,7 @@
 from django.db import models
+from utils.mixins import JSONIntListHandler
 from utils.choices import ExerciseCategoryType
+from accounts.models import CustomUser
 
 class Exercise(models.Model):
     name = models.CharField(
@@ -17,3 +19,34 @@ class Exercise(models.Model):
 
     def __str__(self):
         return self.name
+
+class ExerciseHistory(models.Model):
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+    )
+    user = models.ForeignKey(
+        CustomUser,
+        related_name='exercise_histories',
+        on_delete=models.CASCADE,
+    )
+    exercise_routine = models.JSONField(
+        default=list # list[int]
+    )
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.exercise_routine_handler = JSONIntListHandler(
+            self,
+            'exercise_routine',
+        )
+
+    def __str__(self):
+        return f'{self.user.email}: {self.created_at}'
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=['user','created_at'],
+                name='사용자는 운동을 같은 시각에 한 번만 합니다.',
+            )
+        ]
