b12d6fec5af1bca4129e0c2a361073fcc152b381
Hyerin Sung <hyerinx@ewha.ac.kr>
2025-07-05 03:05:24 +0900

    🔨 Notification 모델 추가 (#20)

--- notifications/models.py
+++ notifications/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from utils.choices import NotificationCategoryType
+from accounts.models import CustomUser
 
-# Create your models here.
+class Notification(models.Model):
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+    )
+    sender = models.ForeignKey(
+        CustomUser,
+        related_name='notification_sendings',
+        on_delete=models.CASCADE,
+    )
+    receiver = models.ForeignKey(
+        CustomUser,
+        related_name='notification_receivings',
+        on_delete=models.CASCADE,
+    )
+    category = models.PositiveSmallIntegerField(
+        choices=NotificationCategoryType.choices,
+    )
+
+    def __str__(self):
+        return f'{self.sender.email}이/가 {self.receiver.email}에게 {NotificationCategoryType(self.category).label}함'
