4456513211f7f0df854d4224b7413148be00f627
Hyerin Sung <hyerinx@ewha.ac.kr>
2025-07-05 02:44:28 +0900

    🔨 Menstruation 모델 추가 (#20)

--- menstruations/models.py
+++ menstruations/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from accounts.models import CustomUser
 
-# Create your models here.
+class Menstruation(models.Model):
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+    )
+    updated_at = models.DateTimeField(
+        auto_now=True,
+    )
+    user = models.ForeignKey(
+        CustomUser,
+        related_name='menstruations',
+        on_delete=models.CASCADE,
+    )
+    start = models.DateField()
+    end = models.DateField()
+
+    def __str__(self):
+        return f'{self.user.email}: {self.start}-{self.end}'
+    
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=['user','start','end'],
+                name='사용자는 월경을 같은 기간에 한 번만 합니다.',
+            )
+        ]
