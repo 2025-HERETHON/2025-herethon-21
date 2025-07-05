b9afe6485b6e9a1dfde33ae373665a9a3381018f
sujin <20241005@sungshin.ac.kr>
2025-07-06 06:22:55 +0900

    + ✨ 로그아웃 (#27)

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import api_signup, api_login
+from .views import main_view, signup_view, login_view, logout_view
 
 app_name = 'accounts'
 
 urlpatterns = [
-    path('signup/', api_signup, name='signup'),
-    path('login/', api_login, name="login"),
+    path('', main_view, name="main"),
+    path('signup/', signup_view, name="signup"),
+    path('login/', login_view, name="login"),
+    path('logout/', logout_view, name="logout")
 ]
