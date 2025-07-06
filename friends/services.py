0e765443717ecc47feb54b5d8aedad950ca18538
sujin <20241005@sungshin.ac.kr>
2025-07-07 08:39:50 +0900

    ✨ 친구 요청 보내기 (#27)

---
+++ friends/services.py
@@ -0,0 +1,17 @@
+from .models import Friend
+from utils.choices import FriendStatusType
+
+class FriendService:
+    @staticmethod
+    def send_request(sender, receiver):
+        if sender == receiver:
+            raise ValueError("본인에겐 친구 요청할 수 없습니다.")
+        
+        if Friend.objects.filter(sender=sender, receiver=receiver).exists():
+            raise ValueError("이미 친구 요청을 보냈습니다.")
+        
+        Friend.objects.create(
+            sender=sender,
+            receiver=receiver,
+            status=FriendStatusType.PENDING
+        )
--- friends/views.py
+++ friends/views.py
@@ -1,17 +1,35 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from utils.choices import FriendStatusType
 from django.db.models import Q
-from .models import Friends
+from .models import Friend
+from accounts.models import CustomUser
+from .services import FriendService
+from django.contrib import messages
 
 @login_required
 def read_friends_list(request):
     user = request.user
-    friends = Friends.objects.filter(
+    friends = Friend.objects.filter(
         Q(sender=user) | Q(receiver=user),
         status=FriendStatusType.ACCEPT
     )
     return render(request, "friend_list.html", {"friends":friends})
+
+@login_required
+def create_send_friends(request):
+    if request.method == "POST":
+        email = request.POST.get("email")
+        try:
+            receiver = CustomUser.objects.get(email=email)
+            FriendService.send_request(request.user, receiver)
+            messages.success(request, f"{email}님에게 친구 요청을 보냈습니다.")
+        except CustomUser.DoesNotExist:
+            messages.error(request, "해당 이메일의 사용자가 존재하지 않습니다.")
+        except ValueError as e:
+            messages.error(request, str(e))
+        return redirect("friends:create_send_friends")
+    return render(request, "friend_request_send.html")
 
 # urlpatterns = [
 #     path('list/', read_friends_list, name="read_friends_list"),
