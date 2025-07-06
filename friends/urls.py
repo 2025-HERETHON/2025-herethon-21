3746a1d7a3ad0e64d19ebcf57bfa4d59fbe8a5d8
sujin <20241005@sungshin.ac.kr>
2025-07-07 08:39:11 +0900

    🔥 불필요한 코드 제거 (#27)

--- friends/urls.py
+++ friends/urls.py
@@ -1,12 +1,9 @@
 from django.urls import path
-from .views import read_friends_list, create_send_friends, read_send_list, create_accept_friend, create_reject_friend
+from .views import read_friends_list, create_send_friends
 
 app_name = 'friends'
 
 urlpatterns = [
     path('list/', read_friends_list, name="read_friends_list"),
     path('send/', create_send_friends, name="create_send_friends"),
-    path('received/', read_send_list, name="read_send_list"),
-    path('accept/<str:username>', create_accept_friend, name="create_accept_friend"),
-    path('reject/<str:username>', create_reject_friend, name="create_reject_friend")
 ]
