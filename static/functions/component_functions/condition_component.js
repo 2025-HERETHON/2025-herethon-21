d02d04bd462b8827d17314f08af23f6266a92847
허윤아 <aquahua@gmail.com>
2025-07-07 10:49:55 +0900

    [0707] 컴포넌트 디자인 수정

--- static/functions/component_functions/condition_component.js
+++ static/functions/component_functions/condition_component.js
@@ -3,6 +3,8 @@ document.addEventListener("DOMContentLoaded", function () {
   const content = document.getElementById("text_input");
   const todaysCondition = document.getElementById("todays_condition");
   const editBtn = document.getElementById("condition_editbtn");
+  const resetBtn = document.getElementById("condition_resetbtn");
+
   let selectedIndex = -1;
   let isEditing = false;
 
@@ -44,22 +46,33 @@ document.addEventListener("DOMContentLoaded", function () {
   }
 
   editBtn.addEventListener("click", function () {
-  if (!isEditing) {
-    content.disabled = false;
-    content.placeholder = "오늘의 일기를 남겨주세요!";
-    isEditing = true;
-    for (let i = 0; i < conditions.length; i++) {
-      conditions[i].classList.remove("disabled");
+    if (!isEditing) {
+      content.disabled = false;
+      content.placeholder = "오늘의 일기를 남겨주세요!";
+      isEditing = true;
+      for (let i = 0; i < conditions.length; i++) {
+        conditions[i].classList.remove("disabled");
+      }
+    } else {
+      alert("저장되었습니다.");
+      content.disabled = true;
+      content.placeholder = "연필 버튼을 눌러 글을 작성하세요!";
+      isEditing = false;
+      for (let i = 0; i < conditions.length; i++) {
+        conditions[i].classList.add("disabled");
+      }
     }
-  } else {
-    alert("저장되었습니다.");
+  });
 
-    content.disabled = true;
-    content.placeholder = "연필 버튼을 눌러 글을 작성하세요!";
-    isEditing = false;
+  resetBtn.addEventListener("click", function () {
+    let ans = confirm("작성 내용을 초기화하시겠습니까?");
+    if (ans){
+      content.value = "";
+    todaysCondition.innerHTML = "";
     for (let i = 0; i < conditions.length; i++) {
-      conditions[i].classList.add("disabled");
+      conditions[i].classList.remove("selected");
     }
+    selectedIndex = -1;
   }
-});
+  });
 });
--- static/functions/component_functions/review_component.js
+++ static/functions/component_functions/review_component.js
@@ -1,7 +1,34 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const editBtn = document.getElementById("editbtn");
+  const editBtn = document.getElementById("editbtn");
+  const resetBtn = document.getElementById("resetbtn");
+  const textarea = document.getElementById("text_input");
 
-    editBtn.addEventListener("click", function () {
-        alert("저장되었습니다.");
+  const stars = document.getElementsByClassName("star_per");
+  const filledSrc = "/static/assets/img/star_purple.png";
+  const emptySrc = "/static/assets/img/star_gray.png";
+
+  for (let i = 0; i < stars.length; i++) {
+    stars[i].addEventListener("click", function () {
+      for (let j = 0; j < stars.length; j++) {
+        stars[j].querySelector("img").src = emptySrc;
+      }
+      for (let j = 0; j <= i; j++) {
+        stars[j].querySelector("img").src = filledSrc;
+      }
     });
-})
+  }
+
+  editBtn.addEventListener("click", function () {
+    alert("저장되었습니다.");
+  });
+
+  resetBtn.addEventListener("click", function () {
+    let ans = confirm("작성한 내용을 초기화하시겠습니까?")
+    if (ans) {
+        textarea.value = "";
+        for (let j = 0; j < stars.length; j++) {
+        stars[j].querySelector("img").src = emptySrc;
+        }
+    }
+  });
+});
