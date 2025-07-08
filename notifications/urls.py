d2995ac8c0074cf483e5b9d20eddaba0cca068ba
Hyerin Sung <hyerinx@ewha.ac.kr>
2025-07-09 03:57:42 +0900

    🔨 read_is_prodded, create_notification 뷰 추가 (#32)
    - 테스트용 코드로 추후 수정해야 합니다.

--- notifications/urls.py
+++ notifications/urls.py
@@ -4,4 +4,6 @@ from .views import *
 app_name = 'notifications'
 
 urlpatterns = [
+    path('create', create_notification, name='create_notification'),
+    path('read/<str:friend_username>', read_is_prodded, name='read_is_prodded'),
 ]
