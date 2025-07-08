1083f411746d633d9abd7bbcd935bafd67efa507
Hyerin Sung <hyerinx@ewha.ac.kr>
2025-07-08 15:45:22 +0900

    🔨 ResponseHelper 수정 (#35)

--- utils/helpers.py
+++ utils/helpers.py
@@ -1,22 +1,37 @@
-from typing import Optional
+from typing import Optional, TypedDict
+from datetime import datetime
 from django.utils import timezone
+from .formatters import format_timestamp
 
 class ResponseHelper:
+    class SuccessType(TypedDict):
+        timestamp: datetime
+        status: int
+        title: str
+        data: Optional[list[dict[str,any]]|dict[str,any]|list[str]]
+        message: Optional[str]
+
+    class ErrorType(TypedDict):
+        timestamp: datetime
+        status: int
+        title: str
+        message: str
+
     @staticmethod
-    def success(status:int, title:str, data:Optional[list[str]|dict[str,any]]=None, message:Optional[str]=None) -> dict[str,any]:
+    def success(status:int, title:str, data:Optional[list[dict[str,any]]|dict[str,any]|list[str]]=None, message:Optional[str]=None) -> SuccessType:
         response = {
-            "timestamp": timezone.localtime(timezone.now()),
+            "timestamp": format_timestamp(timezone.localtime(timezone.now())),
             "status": status,
             "title": title
         }
         if data: response |= {"data": data}
         if message: response |= {"message": message}
         return response
-    
+
     @staticmethod
-    def error(status:int, title:str, message:str) -> dict[str,any]:
+    def error(status:int, title:str, message:str) -> ErrorType:
         return {
-            "timestamp": timezone.localtime(timezone.now()),
+            "timestamp": format_timestamp(timezone.localtime(timezone.now())),
             "status": status,
             "title": title,
             "message": message
