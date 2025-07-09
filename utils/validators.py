7546cbcd4cb8345dea240dc97d47030e7b6633d3
Hyerin Sung <hyerinx@ewha.ac.kr>
2025-07-09 17:01:35 +0900

    🥅 validators HttpResponseException 삭제 및 ClientError로 교체 (#44)

--- utils/validators.py
+++ utils/validators.py
@@ -1,17 +1,12 @@
 from functools import wraps
 from django.db import IntegrityError
-from django.http.response import HttpResponse
-
-class HttpResponseException(Exception):
-    def __init__(self, status:int, message:str):
-        self.response = HttpResponse(message, status=status)
-        super().__init__(message)
+from .exceptions import ClientError
 
 def validate_form(func):
     @wraps(func)
     def wrapper(self, *args, **kwargs):
         if not self.form.is_valid():
-            raise HttpResponseException(400, "필수 값을 누락하였거나, 입력 값이 유효하지 않습니다.")
+            raise ClientError(400, 'Bad Request', '필수 값을 누락하였거나, 입력 값이 유효하지 않습니다.')
         return func(self, *args, **kwargs)
     return wrapper
 
@@ -26,7 +21,7 @@ def validate_not_self(func):
         receiver = bound.arguments.get('receiver')
 
         if sender == receiver:
-            raise HttpResponseException(400, "자신에게는 요청을 수행할 수 없습니다.")
+            raise ClientError(400, 'Bad Request', '자신에게는 요청을 수행할 수 없습니다.')
         return func(self, *args, **kwargs)
     return wrapper
 
@@ -34,7 +29,7 @@ def validate_auth(func):
     @wraps(func)
     def wrapper(self, *args, **kwargs):
         if not self.request.user.is_authenticated:
-            raise HttpResponseException(401, "인증 정보가 없거나 만료되었습니다.")
+            raise ClientError(401, 'Unauthorised', '인증 정보가 없거나 만료되었습니다.')
         return func(self, *args, **kwargs)
     return wrapper
 
@@ -42,7 +37,7 @@ def validate_permission(func):
     @wraps(func)
     def wrapper(self, *args, **kwargs):
         if self.instance.user != self.request.user:
-            raise HttpResponseException(403, "콘텐츠 접근 권한이 없습니다.")
+            raise ClientError(403, 'Forbidden', '콘텐츠 접근 권한이 없습니다.')
         return func(self, *args, **kwargs)
     return wrapper
 
@@ -53,7 +48,5 @@ def validate_unique(func):
             return func(self, *args, **kwargs)
         except IntegrityError as error:
             if 'UNIQUE constraint failed' in str(error):
-                raise HttpResponseException(409, "이미 존재하는 값입니다.")
-            else:
-                raise HttpResponseException(500, str(error))
+                raise ClientError(409, 'Conflict', '이미 존재하는 값입니다.')
     return wrapper
