3db6e7a627e4e10cbfb4fbe9b40856f84c5ce606
Hyerin Sung <hyerinx@ewha.ac.kr>
2025-07-09 17:55:46 +0900

    🥅 ErrorHandlingMiddleware 수정 (#56)
    - messages를 업데이트하는 방식으로 에러 처리

--- utils/middleware.py
+++ utils/middleware.py
@@ -1,7 +1,8 @@
 import logging
 from django.conf import settings
+from django.contrib import messages
 from django.http.request import HttpRequest
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .exceptions import ClientError
 
 logger = logging.getLogger(__name__)
@@ -19,20 +20,24 @@ class ErrorHandlingMiddleware:
 
         if isinstance(exception, ClientError):
             logger.warning(f'{exception.status_code} {exception.status_title}: {exception.message}', exc_info=True)
-            exception_status_code = exception.status_code
-            exception_status_title = exception.status_title
-            exception_message = exception.message
+
+            if (exception.status_code == 400) or (exception.status_code == 409):
+                messages.warning(request, exception.message)
+            else:
+                messages.error(request, exception.message)
+
+            return redirect(request.META.get('HTTP_REFERER', '/'))
         else:
             logger.error(f'{type(exception).__name__}: {exception}', exc_info=True)
+            
             exception_status_code = 500
             exception_status_title = 'Internal Server Error'
             exception_message = str(exception)
 
-        context = {
-            'exception': {
-                'status_code': exception_status_code,
-                'status_title': exception_status_title,
-                'message': exception_message,
-            }
-        }
-        return render(request, 'error.html', context, status=exception_status_code)
+            return render(request, 'error/500.html', status=exception_status_code, context={
+                'exception': {
+                    'status_code': exception_status_code,
+                    'status_title': exception_status_title,
+                    'message': exception_message,
+                }
+            })
