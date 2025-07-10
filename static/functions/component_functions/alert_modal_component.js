ed191427ee4120c5e44ae3836a6d76735db7fd93
5cool9 <pumpkim12345@naver.com>
2025-07-10 16:25:48 +0900

    [0710] 월경 페이지 날짜 관련 수정

--- static/functions/component_functions/alert_modal_component.js
+++ static/functions/component_functions/alert_modal_component.js
@@ -1,12 +1,27 @@
+
 if (typeof window.alertModalCallback === "undefined") {
   window.alertModalCallback = null;
 }
 
-function openAlertModal({ title, imageUrl, onConfirm = null }) {
+function openAlertModal({ title, content, imageUrl, onConfirm = null }) {
+
   document.getElementById('alertModalTitle').textContent = title;
+
   document.getElementById('alertModalImage').src = imageUrl;
 
+  const contentElement = document.getElementById('alertModalContent');
+  if (contentElement) {
+    if (content) {
+      contentElement.textContent = content;
+      contentElement.style.display = "block";
+    } else {
+      contentElement.textContent = "";
+      contentElement.style.display = "none";
+    }
+  }
+
   window.alertModalCallback = onConfirm;
+
   document.getElementById('alertModal').style.display = 'flex';
 }
 
