dd43140d707ecc0426d4f7f060a7edccca7fba19
sujin <20241005@sungshin.ac.kr>
2025-07-12 01:21:19 +0900

    🔨 컨디션 기록 관련 코드 수정 (#85)

--- conditionreviews/urls.py
+++ conditionreviews/urls.py
@@ -4,7 +4,7 @@ from .views import create_ConditionReview, read_ConditionReview, delete_Conditio
 app_name = 'conditionreviews'
 
 urlpatterns = [
-    path('create/', create_ConditionReview, name="create_ConditionReview"),
+    path('create/<str:date_str>', create_ConditionReview, name="create_ConditionReview"),
     path('read/<str:date_str>/', read_ConditionReview, name="read_ConditionReview"),
     path('delete/<str:date_str>/', delete_ConditionReview, name="delete_ConditionReview"),
     path('update/<str:date_str>/', update_ConditionReview, name="update_ConditionReview"),
