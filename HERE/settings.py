0bbb36812cafc2288c7eac3ae38a2b701fe84537
sujin <20241005@sungshin.ac.kr>
2025-07-11 09:39:22 +0900

    🔥 불필요한 세팅 코드 제거 (#27)

--- HERE/settings.py
+++ HERE/settings.py
@@ -74,9 +74,6 @@ WSGI_APPLICATION = 'HERE.wsgi.application'
 
 AUTH_USER_MODEL = 'accounts.CustomUser'
 
-AUTHENTICATION_BACKENDS = [
-    'django.contrib.auth.backends.ModelBackend',
-]
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
