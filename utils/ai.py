6916b97dede69d17100fa0b46192124365add743
Hyerin Sung <hyerinx@ewha.ac.kr>
2025-07-11 12:01:59 +0900

    🔨 Gemini API 연결 수정 (#31)

--- utils/ai.py
+++ utils/ai.py
@@ -4,13 +4,17 @@ from env import GOOGLE_API_KEY
 
 client = genai.Client(api_key=GOOGLE_API_KEY)
 
-def gemini(system_instruction, contents):
-    response = client.models.generate_content(
-        model="gemini-2.5-flash",
-        config=types.GenerateContentConfig(
-            thinking_config=types.ThinkingConfig(thinking_budget=0),
-            system_instruction=system_instruction,
-        ),
-        contents=contents,
-    )
-    return response
+class Gemini:
+    def __init__(self, system_instruction):
+        self.system_instruction = system_instruction
+
+    def request(self, contents):
+        response = client.models.generate_content(
+            model="gemini-2.5-flash",
+            config=types.GenerateContentConfig(
+                thinking_config=types.ThinkingConfig(thinking_budget=0),
+                system_instruction=self.system_instruction,
+            ),
+            contents=contents,
+        )
+        return response.text
