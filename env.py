3e9efbf07d89f92e9f88286de3c18d15d005df47
5cool9 <pumpkim12345@naver.com>
2025-09-14 18:14:26 +0900

    달력 js 수정

--- env.py
+++ env.py
@@ -2,14 +2,11 @@ from pathlib import Path
 import environ
 import os
 
-env = environ.Env(DEBUG=(bool, False))
-
 BASE_DIR = Path(__file__).resolve().parent
-
+env = environ.Env(DEBUG=(bool, False))
 environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
 DEBUG = env('DEBUG')
 SECRET_KEY = env('SECRET_KEY')
-DATABASE_DEFAULT = env.db()
-
+DATABASE_DEFAULT = env.db_url('DATABASE_URL')
 GOOGLE_API_KEY = env('GOOGLE_API_KEY')
