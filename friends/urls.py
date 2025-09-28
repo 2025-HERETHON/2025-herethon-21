be323920544fa79fce975791bb1ae549fba66b5b
Hyerin Sung <145767486+hyerindev@users.noreply.github.com>
2025-09-28 19:25:31 +0900

    코드 포맷팅 (#123)
    * 🎨 import문 정리 (#89)
    
    * 🎨 불필요한 공백 및 빈줄 제거 (#89)

--- friends/urls.py
+++ friends/urls.py
@@ -1,6 +1,4 @@
 from django.urls import path
-from django.conf import settings
-from django.conf.urls.static import static
 from .views import read_friends_list, create_send_friends, read_receive_list, create_accept_friend, create_reject_friend, read_friend_detail
 
 app_name = 'friends'
@@ -11,5 +9,5 @@ urlpatterns = [
     path('received/', read_receive_list, name="read_send_list"),
     path('accept/<str:id>/', create_accept_friend, name="create_accept_friend"),
     path('reject/<str:id>/', create_reject_friend, name="create_reject_friend"),
-    path('detail/<str:id>/', read_friend_detail, name="read_friend_detail"),  
+    path('detail/<str:id>/', read_friend_detail, name="read_friend_detail"),
 ]
