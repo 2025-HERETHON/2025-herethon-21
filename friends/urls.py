6d8ae99ca29246c8c1101bfdae21f6e1559bfeba
Hyerin Sung <145767486+hyerindev@users.noreply.github.com>
2025-09-28 19:26:51 +0900

    친구 수락/거절 시 기존 알림 삭제 및 알림 추가 (#119)
    * 🎨 friend_id 변수 이름 변경 (#89)
    
    * 🐛 알림 조회 기능 수정 (#89)
    
    - 내가 알림을 보내는 주체인 경우 2,3,4에 대하여 내 알림 목록에 포함
    
    * 🐛 콕 찌르기 여부 조회 기능에서 잘못된 쿼리 조건 수정 (#89)
    
    - date는 date끼리 비교해야 함
    
    * 🔥 알림 추가 기능 반환값 삭제 (#89)
    
    * ✨ 알림 삭제 기능 추가 (#89)
    
    * 🐛 친구 수락/거절 시 기존 알림 삭제 및 알림 추가 (#89)
    
    * 🐛 알림 목록 조회 기능 content 조사 수정 (#89)

--- friends/urls.py
+++ friends/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path('list/', read_friends_list, name="read_friends_list"),
     path('send/', create_send_friends, name="create_send_friends"),
     path('received/', read_receive_list, name="read_send_list"),
-    path('accept/<str:id>/', create_accept_friend, name="create_accept_friend"),
-    path('reject/<str:id>/', create_reject_friend, name="create_reject_friend"),
+    path('accept/<str:notification_id>/', create_accept_friend, name="create_accept_friend"),
+    path('reject/<str:notification_id>/', create_reject_friend, name="create_reject_friend"),
     path('detail/<str:id>/', read_friend_detail, name="read_friend_detail"),
 ]
